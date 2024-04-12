9a491cbd98c1b7ac5cf2680ec8307448a848a4d1
ranadaivan123 <ranadaivanjericho78@gmail.com>
2024-04-12 20:40:05 +0800

    delete and edit

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,29 +1,28 @@
 <template>
-    <div>
-      <div>{{ product.name }}</div>
-      <div>{{ product.description }}</div>
-      <div>{{ product.price }}</div>
-      <button @click="edit">Edit</button>
-      <button @click="remove">Delete</button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ProductItem',
-    props: ['product'],
-    methods: {
-      edit() {
-        this.$emit('edit', this.product);
-      },
-      remove() {
-        this.$emit('delete', this.product);
-      },
+  <div>
+    <div>{{ product.name }}</div>
+    <div>{{ product.description }}</div>
+    <div>{{ product.price }}</div>
+    <button @click="edit">Edit</button>
+    <button @click="remove">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductItem',
+  props: ['product'],
+  methods: {
+    edit() {
+      this.$emit('edit', this.product);
     },
-  };
-  </script>
-  
-  <style>
-  /* Add your component's styles here */
-  </style>
-  
+    remove() {
+      this.$emit('delete', this.product);
+    },
+  },
+};
+</script>
+
+<style>
+/* Add your component's styles here */
+</style>
